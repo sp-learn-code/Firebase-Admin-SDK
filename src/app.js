a1e3051b662108a1510d1ada1a3155f153fd9fd7
Jorge Zambrano <joraazam@espol.edu.ec>
2022-03-13 03:38:53 -0500

    Actualizacion y eliminado de datos de usuarios

--- src/app.js
+++ src/app.js
@@ -6,14 +6,17 @@ const path = require('path')
 
 
 const app = express()
+//Configuracion de morgan, con el parametro de morgan dev, en el que indica que si se realiza una peticion, la podemos ver por consola
 app.use(morgan('dev'))
 
 //Estas dos lineas hace que cuando yo envie un dato a traves de post (request body) el lo entendera
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+//Accede a todas las rutas 
 app.use(require('./routes/index'))
 
+//Hace publica la carpeta en el navegador (aplicacion cliente), es decir, puede acceder a sus archivos
 app.use(express.static(path.join(__dirname,'public')))
 
 module.exports = app
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,23 @@
-const {Router} = require('express')
-const {db} = require('../firebase')
+const { Router } = require('express')
+const { db } = require('../firebase')
 
+//Router necesita inicializarse
 const router = Router()
 
-router.get('/contacts', async (req,res)=>{
+router.get('/contacts', async (req, res) => {
     const querySnapshot = await db.collection('contacts').get()
-    
+
     const contacts = querySnapshot.docs.map(doc => ({
-            id: doc.id,
-        ...doc.data() 
+        id: doc.id,
+        ...doc.data()
     }))
     console.log(contacts)
     res.send('Hello')
-}) 
+})
 
-router.post('/new-contact', async (req,res)=> {
-    const {firstname,lastname,email,phone} = req.body
+router.post('/new-contact', async (req, res) => {
+    //req.body es la informacion que nos envia el cliente 
+    const { firstname, lastname, email, phone } = req.body
 
     await db.collection('contacts').add({
         firstname,
@@ -25,6 +27,13 @@ router.post('/new-contact', async (req,res)=> {
     })
 
     res.send("New contact created")
+})
+
+router.get('/edit-contact/:id', async (req, res) => {
+    const doc = await db.collection('contacts').doc(req.params.id).get()
+    console.log({ id: doc.id, ...doc.data() })
+
+    res.send("Edit contact")
 })
 
 module.exports = router  
